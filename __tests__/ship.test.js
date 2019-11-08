bcff7cc9d7ada552744242da2af23d4bb2201f62
lucya3584 <lucya3584@yahoo.co.uk>
2019-11-08 16:35:06 +0000

    returns an object. Passes

---
+++ __tests__/ship.test.js
@@ -0,0 +1,14 @@
+const Ship = require('../src/Ship');
+
+describe('Ship', () => {
+    it('returns an object', () => {
+        expect(new Ship()).toBeInstanceOf(Object);
+    });
+    
+    it('has a starting port', () => {
+    const ship = new Ship(Dover)
+    expect(ship.startingPort).toBe('Dover')
+    });
+
+    let cruiseShip = new Ship()
+    });
