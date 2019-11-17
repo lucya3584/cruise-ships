a435e54671793b0f066f0f1505f4689e7af4bf36
lucya3584 <lucya3584@yahoo.co.uk>
2019-11-17 17:59:18 +0000

    reduced port name duplication, made DRY

--- __tests__/port.test.js
+++ __tests__/port.test.js
@@ -1,31 +1,41 @@
 const Port = require('../src/Port.js');
 
 describe('Port', () => {
-  it('returns an object', () => {
-    expect(new Port()).toBeInstanceOf(Object)
+  describe('port name', () => {
+    let port;
+    beforeEach(() => {
+      port = new Port('Dover')
+    });
+
+    it('returns an object', () => {
+      expect(new Port()).toBeInstanceOf(Object)
+    });
+
+    it('has a name propery', () => {
+
+      expect(port.name).toBe('Dover')
+    });
+
+    it('can add a ship', () => {
+
+      const ship = {};
+
+      port.addShip(ship);
+
+      expect(port.ships).toContain(ship);
+    });
+
+    it('can remove a ship', () => {
+
+      const titanic = {}
+      const queenMary = {}
+
+      port.addShip(titanic)
+      port.addShip(queenMary)
+      port.removeShip(queenMary)
+
+      expect(port.ships).toEqual([titanic])
+    });
+
   });
-  it('has a name propery', () => {
-    const port = new Port('Dover')
-    expect(port.name).toBe('Dover')
-  });
-  it('can add a ship', () => {
-    const port = new Port('Dover')
-    const ship = {};
-
-    port.addShip(ship);
-
-    expect(port.ships).toContain(ship);
-  });
-  it('can remove a ship', () => {
-    const port = new Port('Dover')
-    const titanic = {}
-    const queenMary = {}
-
-    port.addShip(titanic)
-    port.addShip(queenMary)
-    port.removeShip(queenMary)
-
-    expect(port.ships).toEqual([titanic])
-  });
-
 });
